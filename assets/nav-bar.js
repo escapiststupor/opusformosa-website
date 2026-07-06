f042c0aa6d62027c39838ee917500e3de5c72eee
Phyllis Yen <pyen@nvidia.com>
2026-07-07 01:07:16 +0800

    Update website - 2026-07-07 01:07:16

--- assets/nav-bar.js
+++ assets/nav-bar.js
@@ -46,6 +46,7 @@
     if (path === "artists.html") return "artists";
     if (path === "partners.html") return "partners";
     if (path === "friends.html") return "friends";
+    if (path === "2026-friends.html" || path === "2026-friends" || path === "2026-friends/") return "friends";
     return "index";
   }
 
